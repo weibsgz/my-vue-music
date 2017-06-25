91866b4a5a7a5f2f179da6c5bfd712b8f649dc10
weibin <weibsgz@126.com>
2017-06-25 22:35:34 +0800

    home

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input ref="query" v-model="query" :placeholder="placeholder" class="box" />
+    <i @click="clear" v-show="query" class="icon-dismiss"></i>
+  </div>
+</template>
+
+<script >
+    export default{
+        props:{
+            placeholder:{
+                type:String,
+                default:'搜索歌曲、歌手'
+            }
+        },
+        data(){
+            return {
+                query : ""
+            }
+        },
+        methods:{
+            clear(){
+                this.query = "";
+            },
+            setQuery(query){
+                this.query = query
+            }
+        },
+        //这里watch query的变化 向上派发出去
+        created(){
+            this.$watch('query',(newquery) => {
+                this.$emit('query',newquery)
+            })
+        }
+    }
+</script>
+
+<style scoped lang="less">
+  @import '~common/css/variables.less';
+  .search-box{
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 0 6px;
+    height: 40px;
+    background: @color-highlight-background;
+    border-radius: 6px;
+    .icon-search{
+      font-size: 24px;
+      color: @color-background;
+    }      
+    .box{
+      flex: 1;
+      margin: 0 5px;
+      line-height: 18px;
+      background: @color-highlight-background;
+      color: @color-text;
+      font-size: @font-size-medium;
+      &::placeholder{
+        color: @color-text-d 
+      }        
+    }      
+    .icon-dismiss{
+      font-size: 16px;
+      color: @color-background;
+    }
+      
+  }
+
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,11 +1,142 @@
 <template>
-  <div>
-    查询页面
+  <div class="search">
+    <div class="search-box-wrapper">
+        <SearchBox ref="searchBox" @query = 'onQueryChange'></SearchBox>
+    </div>
+    <div class="shortcut-wrapper" v-show="!query">
+        <div class="shortcut">
+            <div class="hot-key">
+                <h1 class="title">热门搜索</h1>
+                <ul>
+                    <li @click="addQuery(item.k)" class="item" v-for="(item,index) in hotkey">
+                        <span>{{item.k}}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>        
+    </div>    
+    <div class="search-result" v-show="query">
+            <Suggest :query='query'></Suggest>
+        </div>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
+    import SearchBox from 'base/search-box/search-box'
+    import {getHotKey} from 'api/search'
+    import {ERR_OK} from 'api/config'
+    import Suggest from 'components/suggest/suggest'
+    export default {
+        created(){
+            this._getHotKey();
+            let test = {a:1,b:2}
+            let arr = []
+            
+            arr.push(...test)
+            console.log(arr)
+        },
+        data(){
+            return {
+                hotkey:[],
+                query:''
+            }
+        },
+        methods:{
+            _getHotKey(){
+                getHotKey().then((res) =>{
+                    if(res.code === ERR_OK){
+                        console.log(res.data.hotkey)
+                        this.hotkey = res.data.hotkey.slice(0,10)
+                    }
+                })
+            },
+            addQuery(query){
+                this.$refs.searchBox.setQuery(query)
+            },
+            onQueryChange(query){
+                this.query = query
+            }
+        },
+        components:{
+            SearchBox,
+            Suggest
+        }
+    }
 </script>
 
 <style scoped lang="less">
+  @import '~common/css/variables.less';
+  @import '~common/css/mixins.less';
+  
+  .search{
+    .search-box-wrapper{
+      margin: 20px
+    }
+    .shortcut-wrapper{
+      position: fixed;
+      top: 178px;
+      bottom: 0;
+      width: 100%;
+      .shortcut{
+        height: 100%;
+        overflow: hidden;
+        .hot-key{
+            margin: 0 20px 20px 20px;
+          .title{
+             margin-bottom: 20px;
+            font-size: @font-size-medium;
+            color: @color-text-l;
+          }           
+          .item{
+            display: inline-block;
+            padding: 5px 10px;
+            margin: 0 20px 10px 0;
+            border-radius: 6px;
+            background: @color-highlight-background;
+            font-size: @font-size-medium;
+            color: @color-text-d;
+          }
+            
+        }
+          
+        .search-history{
+            position: relative;
+          margin: 0 20px;
+          .title{
+             display: flex;
+            align-items: center;
+            height: 40px;
+            font-size: @font-size-medium;
+            color: @color-text-l;
+            .text{
+              flex: 1;
+            }
+            .clear{
+              .extend-click() ;
+              .icon-clear{
+                 font-size: @font-size-medium;
+                color: @color-text-d ;
+              }
+               
+            }
+          }
+        }
+          
+           
+              
+      }        
+    }
+     
+    .search-result{
+        position: fixed;
+      width: 100%;
+      top: 178px;
+      bottom: 0 ;
+    }
+     
+  }
+    
+
+
+  
 </style>
